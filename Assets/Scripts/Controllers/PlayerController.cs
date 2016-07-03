43431490bdae062b33a44a8f3fd7774cc48199a5
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-07-03 17:21:04 -0400

    FirstCommit?

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;  //We can controll the speed of the player here.
+    public Rigidbody rb;
+
+    public void PlayerMove(float xAxis)
+    {
+        //if you're not moving Left or Right
+        if (xAxis != 0)
+        {
+            if (xAxis > 0)
+            {
+                rb.AddForce(transform.right * playerSpeed);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerController playerController;
+    float xAxis = 0; // 1 = right, -1 = left
+
+    void Awake ()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        if (xAxis != 0)
+        {
+            playerController.PlayerMove(xAxis);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadLevel();
+        }
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Loads the current scene again
+    }
+
+}
